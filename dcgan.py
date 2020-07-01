bd07ac1c0b30e56cba1d1bd7f3ec21cb9d4f09a0
noajam <nrj006@uark.edu>
2020-07-01 09:38:27 -0500

    Commiting remaining unstaged files

--- dcgan.py
+++ dcgan.py
@@ -1,9 +1,39 @@
 import torch
 import torch.nn as nn
 
-print("Have not imported models yet.")
-from models import discriminator, generator
-print("Have imported models.")
+from config.loadJSON import read_json
 
-gnet = generator.Generator(0, 100, 64, 3)
-dnet = discriminator.Discriminator(0, 3, 64)
+from models import discriminator, generator
+
+from utils.weights import weights_init
+
+from datasets.dataset import *
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# Set configuration dictionary
+cfg = read_json("config/config.json")
+
+# Set dataset and dataloader
+dataset, dataloader = None, None
+if cfg['dataset'] == 'celebA':
+    dataset, dataloader = celebA()
+elif cfg['dataset'] == 'paintings':
+    dataset, dataloader == paintings()
+else:
+    raise ValueError("Dataset specified in config/config.json is not implemented.")
+
+netG = generator.Generator(cfg['ngpu'], cfg['nz'], cfg['ngf'], cfg['nc'])
+netG.apply(weights_init)
+
+netD = discriminator.Discriminator(cfg['ngpu'], cfg['nc'], cfg['ndf'])
+netD.apply(weights_init)
+
+# define loss
+# Binary Cross Entropy Loss
+# make an optimizer
+# Adam optimizers for generator and discriminator
+
+# train the discriminator
+# train the generator
