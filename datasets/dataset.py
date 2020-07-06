3c1a81728e02cdb5cb99462bd1a1a50444bdea75
Noah James <nrj006@uark.edu>
2020-07-06 10:15:15 -0500

    Added CUDA compatibility, better batch printing

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -3,13 +3,13 @@ from torch.utils.data import DataLoader
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
+from pathlib import Path
 
 
 
+def celeba(batchSize, nw):
 
-def celeba(batchSize):
-
-    dataset = datasets.ImageFolder(root="data/celebA",
+    dataset = datasets.ImageFolder(root=Path("data/celeba"),
                                   transform=transforms.Compose([
                                       transforms.Resize(64),
                                       transforms.CenterCrop(64),
@@ -18,13 +18,13 @@ def celeba(batchSize):
                                                            (0.5, 0.5, 0.5))
                                       ]))
 
-    dataloader = DataLoader(dataset, batchSize, shuffle=True, num_workers=2)
+    dataloader = DataLoader(dataset, batchSize, shuffle=True, num_workers=nw)
     
     return dataset, dataloader
 
 
-def paintings():    
-    dataset = datasets.ImageFolder(root="data/paintings/VVG",
+def paintings(batchSize, nw):    
+    dataset = datasets.ImageFolder(root=Path("data/paintings/VVG"),
                                   transform=transforms.Compose([
                                       transforms.Resize(64),
                                       transforms.CenterCrop(64),
@@ -33,18 +33,18 @@ def paintings():
                                                            (0.5, 0.5, 0.5))
                                       ]))
     
-    dataloader = DataLoader(dataset, 32, shuffle=True, num_workers=2)
+    dataloader = DataLoader(dataset, batchSize, shuffle=True, num_workers=nw)
     
     return dataset, dataloader
 
-def mnist(batchSize):
-    dataset = datasets.MNIST(root="data/mnist", download=True,
+def mnist(batchSize, nw):
+    dataset = datasets.MNIST(root=Path("data/mnist"), download=True,
                            transform=transforms.Compose([
                                transforms.Resize(64),
                                transforms.ToTensor(),
                                transforms.Normalize((0.5,), (0.5,)),
                                ]))
     
-    dataloader = DataLoader(dataset, batchSize, shuffle=True, num_workers=2)
+    dataloader = DataLoader(dataset, batchSize, shuffle=True, num_workers=nw)
     
     return dataset, dataloader
